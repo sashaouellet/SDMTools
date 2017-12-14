8e310b660c1aff0e87212e9ded2799492877fc5d
sashaouellet <spaouellet@me.com>
2017-12-13 18:00:03 -0700

    Another fix for double logging

--- python/sdm/houdini/__init__.py
+++ python/sdm/houdini/__init__.py
@@ -11,15 +11,21 @@ if not os.path.exists(logDir):
 	os.makedirs(logDir)
 
 logHandler = RotatingFileHandler(os.path.join(logDir, 'sdm_tools.log'), maxBytes=10485760, backupCount=5, encoding='utf8')
+loggers = {}
 
 def getLogger(moduleName):
+	logger = loggers.get(moduleName)
+
+	if logger:
+		return logger
+
 	logger = logging.getLogger(moduleName)
 	formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(name)s:%(funcName)s: %(message)s', '%Y-%m-%d %H:%M:%S')
 
 	logHandler.setFormatter(formatter)
 	logger.addHandler(logHandler)
 
-	logger.propagate = False
+	loggers[moduleName] = logger
 
 	return logger
 
