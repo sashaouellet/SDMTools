ca8295e7038d03fd3a015096e16474d0f243da11
sashaouellet <spaouellet@me.com>
2017-12-15 18:22:09 -0700

    New API for getting the current desktop's scene viewer and viewport of a specific type

--- python/sdm/houdini/camera.py
+++ python/sdm/houdini/camera.py
@@ -20,6 +20,45 @@ def getCameras():
 	"""
 	return [c for c in hou.node('/').allSubChildren() if c.type().name() == 'cam']
 
+def getSceneViewer():
+	"""Gets the scene viewer for the current desktop
+
+	Returns:
+	    hou.SceneViewer: The current desktop's scene viewer, or None if there is
+	    	no such pane tab created for the current desktop
+	"""
+	sceneViewer = hou.ui.curDesktop().paneTabOfType(hou.paneTabType.SceneViewer)
+
+	if not sceneViewer:
+		logger.warning('Unable to locate pane tab "SceneViewer" in current desktop')
+		return None
+
+	return sceneViewer
+
+def getCurrentViewport(viewportType):
+	"""Gets the current viewport of the current desktop's scene viewer pane
+
+	Args:
+	    viewportType (hou.geometryViewportType): The type of viewport to retrieve from
+	    	the current hou.SceneViewer
+
+	Returns:
+	    hou.GeometryViewport: The current scene viewer's vewport of the given type, or None
+	    	if that type doesn't exist or the scene viewer could not be found
+	"""
+	sceneViewer = getSceneViewer()
+
+	if not sceneViewer:
+		return None
+
+	viewport = [vp for vp in sceneViewer.viewports() if vp.type() == viewportType]
+
+	if not viewport:
+		logger.warning('Unable to locate viewport of type {} from scene viewer: {}'.format(viewportType, sceneViewer))
+		return None
+
+	return viewport[0]
+
 def flipbook(camera, output=None, frameRange=None):
 	"""Outputs a flipbook animation from the given camera
 
@@ -34,11 +73,10 @@ def flipbook(camera, output=None, frameRange=None):
 
 	assert camera.type().name() == 'cam', 'Node is not a camera: {}'.format(camera.path())
 
-	sceneViewer = hou.ui.curDesktop().paneTabOfType(hou.paneTabType.SceneViewer)
+	sceneViewer = getSceneViewer()
 
 	if not sceneViewer:
 		hou.ui.displayMessage('Could not find Scene Viewer pane tab, please create it and try again', title='Flipbook Error', severity=hou.severityType.Error)
-		logger.warning('Unable to locate pane tab "SceneViewer" in current desktop')
 		return
 
 	frameStart = int(sceneViewer.flipbookSettings().frameRange()[0])
@@ -53,11 +91,10 @@ def flipbook(camera, output=None, frameRange=None):
 	else:
 		logger.debug('Using flipbook settings for frame range: {}-{} with increment: {}'.format(frameStart, frameEnd, frameInc))
 
-	viewport = [vp for vp in sceneViewer.viewports() if vp.type() == hou.geometryViewportType.Perspective][0]
+	viewport = getCurrentViewport(hou.geometryViewportType.Perspective)
 
 	if not viewport:
 		hou.ui.displayMessage('Could not find the "Persp" viewport', title='Flipbook Error', severity=hou.severityType.Error)
-		logger.warning('Unable to locate "Persp" viewport from scene viewer: {}'.format(sceneViewer))
 		return
 
 	viewportFullName = '{}.{}.world.{}'.format(hou.ui.curDesktop().name(), sceneViewer.name(), viewport.name())
@@ -74,4 +111,3 @@ def flipbook(camera, output=None, frameRange=None):
 
 	logger.debug('Executing HScript: {}'.format(command))
 	hou.hscript(command)
-	logger.info('Done flipbooking')
